bd733f0776911ac77f00e9779638b416d7780b52
4nx13ty8 <ialxstrm@gmail.com>
2024-12-07 14:37:27 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://8f35be2e-37f8-40c2-80b1-eccf3bc5bd67.serverhub.praktikum-services.ru/"
+CREATE_ORDER_PATH = "/api/v1/orders"
+FIND_ORDER_FROM_TRACK_PATH = "/api/v1/orders/track?t="
